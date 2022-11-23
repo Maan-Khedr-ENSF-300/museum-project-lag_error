4ccdc2daf74feefb7a8ae8f210b027a194bfdd36
lokkochan <chunlok.chan@ucalgary.ca>
2022-11-23 16:07:23 -0700

    Having functions in

--- Museum.py
+++ Museum.py
@@ -7,3 +7,53 @@ museum=mysql.connector.connect(
     passwd="password",
     database="museum"
 )
+
+users=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+    database="users"
+)
+
+def log_in():
+    pass
+
+def add_user():
+    pass
+
+def edit_user():
+    pass
+
+def block_user():
+    pass
+
+def modify_database():
+    pass
+
+def admin_menu():
+    pass
+
+def admin():
+    pass
+
+def add_tuples():
+    pass
+
+def modify_info():
+    pass
+
+def data_entry_menu():
+    pass
+
+def data_entry():
+    pass
+
+def end_user_menu():
+    pass
+
+def end_user():
+    pass
+
+
+def main():
+    pass
