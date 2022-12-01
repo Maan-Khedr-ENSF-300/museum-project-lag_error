b9297fbea78f22715ed009168f67fdf46f2eb3f0
AidanW86 <aidan.wong1@ucalgary.ca>
2022-12-01 10:45:19 -0700

    Adjusted sql users to new file

---
+++ museumusers.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS museumusers;
+CREATE DATABASE museumusers; 
+USE museumusers;
+
+DROP TABLE IF EXISTS Users
+CREATE TABLE Users(
+	Username		varchar(30) not null,
+	Password		varchar(30) not null,
+	Access_Level	int(20) not null,
+	primary key(Username)
+);
+INSERT INTO Users(Username, Password, Access_Level)
+VALUES
+('Bob', 'Abc123p34', '1'),
+('Tim', 'BlueBigB', '1'),
+('Zack', '13#%@KFFc', '2')
+
+
+DROP TABLE IF EXISTS Block_List
+CREATE TABLE Block_List(
+	Username		varchar(30) not null,
+	Blocked_Date	varchar(20) not null,
+	foreign key(Username) references Users(Username),
+)
+INSERT INTO Block_List(Username, Blocked_Date)
+VALUES
+('Joe', 'May 4 2021'),
+('Zoe', 'Jan 8 2019'),
+('ThatGuy', 'Feb 28 2020')
